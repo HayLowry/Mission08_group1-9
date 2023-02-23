e096994e39f35b9730d6b4c8efe61c0477dd8304
haylowry <haylow77@gmail.com>
2023-02-23 08:22:16 -0700

    Add project files.

---
+++ Mission08_group1-9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_group1_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
