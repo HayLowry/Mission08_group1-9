7e595199fcf991474cbf22218062b984b0738ba8
haylowry <haylow77@gmail.com>
2023-02-23 09:42:01 -0700

    Create Database Models

---
+++ Mission08_group1-9/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_group1_9.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskTitle { get; set; }
+        public string DueDate { get; set; }
+        public bool Completed { get; set; }
+
+        // Build foreign key relationship
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Mission08_group1-9/Models/TaskContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_group1_9.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        // seeding data
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    CategoryName = "Home"
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    CategoryName = "School"
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    CategoryName = "Work"
+                },
+                new Category
+                {
+                    CategoryId = 4,
+                    CategoryName = "Church"
+                }
+                );
+
+            mb.Entity<Task>().HasData(
+                new Task
+                {
+                    TaskId = 1,
+                    TaskTitle = "My First Task",
+                    DueDate = "2023-02-25",
+                    Completed = false,
+                    CategoryId = 1
+                }
+            );
+        }
+    }
+}
